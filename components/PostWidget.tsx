63b607ff3859c35f81c99fdfec10b210d5ef40da
alessandrobarreto <alessandrobarreto1201@gmail.com>
2022-01-11 15:17:07 +0000

    wip: fix getRecentPosts service

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../services";
 
-export const PostWidget = () => {
-    return (
-        <div>
-            PostWidget
+export const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
+    }
+  }, [slug]);
+  console.log(relatedPosts);
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4 ">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full ">
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              height={60}
+              width={60}
+              className="align-midle rounded-full"
+            />
+          </div>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
